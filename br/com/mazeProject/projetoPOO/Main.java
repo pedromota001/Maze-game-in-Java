3bffd8d2cfa727e67646aa7dab7307be199ebe1c
Pedro Mota <pedromotalindoso@gmail.com>
2024-05-23 15:59:00 -0300

    verificacao da funcao de mover aventureiro

--- br/com/mazeProject/projetoPOO/Main.java
+++ br/com/mazeProject/projetoPOO/Main.java
@@ -1,23 +1,109 @@
 package br.com.mazeProject.projetoPOO;
-import java.util.Scanner;
-import br.com.mazeProject.projetoPOO.Labirinto;
 
-public class Main {
-    static void labirinto(){
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-    }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+public class Main extends JFrame {
+    private JPanel controlPanel;
+    private JTextArea messageArea;
+    private Aventureiro aventureiro;
+    private Labirinto labirinto;
 
-        System.out.println("Digite o valor do labirinto: ");
-        int tamanhoLab = scanner.nextInt();
-
-        Labirinto labirinto = new Labirinto(tamanhoLab);
-
+    public Main() {
+        labirinto = new Labirinto(10); // Tamanho do labirinto 10x10
         labirinto.gerarLabirinto();
+        aventureiro = new Aventureiro("Aventureiro", 0, 0); // Posição inicial do aventureiro
 
-        for(int i = 0; i < tamanhoLab*tamanhoLab; i++){
+        setTitle("Maze Game");
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
 
+        // Área de Mensagens
+        messageArea = new JTextArea();
+        messageArea.setEditable(false);
+        add(new JScrollPane(messageArea), BorderLayout.CENTER);
+
+        // Painel de Controle
+        controlPanel = new JPanel();
+        controlPanel.setLayout(new GridLayout(3, 3));
+
+        JButton upButton = new JButton("↑");
+        JButton downButton = new JButton("↓");
+        JButton leftButton = new JButton("←");
+        JButton rightButton = new JButton("→");
+
+        upButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                moveAventureiro(0, -1);
+            }
+        });
+
+        downButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                moveAventureiro(0, 1);
+            }
+        });
+
+        leftButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                moveAventureiro(-1, 0);
+            }
+        });
+
+        rightButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                moveAventureiro(1, 0);
+            }
+        });
+
+        controlPanel.add(new JLabel(""));
+        controlPanel.add(upButton);
+        controlPanel.add(new JLabel(""));
+        controlPanel.add(leftButton);
+        controlPanel.add(new JLabel(""));
+        controlPanel.add(rightButton);
+        controlPanel.add(new JLabel(""));
+        controlPanel.add(downButton);
+        controlPanel.add(new JLabel(""));
+
+        add(controlPanel, BorderLayout.SOUTH);
+
+        setVisible(true);
+    }
+
+    private void moveAventureiro(int dx, int dy) {
+        int newX = aventureiro.getPosicaox() + dx;
+        int newY = aventureiro.getPosicaoy() + dy;
+
+        if (newX >= 0 && newX < labirinto.getEstrutura().length && newY >= 0 && newY < labirinto.getEstrutura()[0].length) {
+            aventureiro.setPosicaox(newX);
+            aventureiro.setPosicaoy(newY);
+
+            labirinto.exibicaoEmovimentacaoAventureiro(aventureiro);
+
+            // Limpar a área de mensagens antes de exibir as novas mensagens
+            messageArea.setText("");
+            // Exibir todas as mensagens no GUI
+            for (String message : labirinto.getMensagens()) {
+                messageArea.append(message + "\n");
+            }
         }
     }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Main();
+            }
+        });
+    }
 }
+
