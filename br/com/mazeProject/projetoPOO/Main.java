6f40465b4ab232226552c5be6db085156ca78549
luisgustavo <luisgosampaio@gmail.com>
2024-05-28 08:17:19 -0300

    atualização do try catch execption

--- br/com/mazeProject/projetoPOO/Main.java
+++ br/com/mazeProject/projetoPOO/Main.java
@@ -1,8 +1,12 @@
 package br.com.mazeProject.projetoPOO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 import br.com.mazeProject.projetoPOO.Labirinto;
+
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -14,33 +18,45 @@ import java.awt.event.ActionListener;
 public class Main {
 
     public static void main(String[] args) {
+        //inicializando o aventureiro
         Aventureiro aventureiro = new Aventureiro("Aventureiro" , 0, 0);
-        Scanner scanner = new Scanner(System.in);
+
+        //criaçao do frame e do panel do gui swingx
         JFrame frame = new JFrame();
         JPanel panel = new JPanel(new GridBagLayout());
 
-        JLabel label2 = new JLabel();
+        //criaçao da imagem e do arquivo imagem, o primeiro utilizado para dar imagem propriamente dito e o imagefile para verificação se existe esse arquivo
         ImageIcon image = new ImageIcon("../Maze-game-in-Java/br/com/mazeProject/projetoPOO/fotos/gameover.png");
-
         File imageFile = new File("../Maze-game-in-Java/br/com/mazeProject/projetoPOO/fotos/gameover.png");
 
-        if(!imageFile.exists()){
-            System.out.println("Imagem não encontrada!");
-        } else{
-            System.out.println("Imagem carregada com sucesso!");
+        //tratamento de excessao
+        try{
+            if(!imageFile.exists()){
+                System.out.println("Imagem não encontrada!");
+            } else{
+                BufferedImage imagem = ImageIO.read(imageFile);
+                if(imagem == null){
+                    System.out.println("Erro ao carregar à imagem: formato não suportado!");
+                } else{
+                    System.out.println("Imagem carregada com sucesso!");
+                }
+
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao abrir o arquivo da imagem" + e.getMessage());
         }
 
+        //criaçao do label para messagens
         JLabel messageLabel = new JLabel("", SwingConstants.CENTER);
         messageLabel.setPreferredSize(new Dimension(200,200));
 
+        //definição do tamanho do butao e criacao de aba para retorno de mensagens com relação ao labirinto
         Dimension buttonSize = new Dimension(50,50);
-        Dimension gameOverSize = new Dimension(150, 150);
         JTextArea messageArea = new JTextArea(5,20);
-
-
         messageArea.setEditable(false);
         JScrollPane scrollPane = new JScrollPane(messageArea);
 
+        //configuraçoes de butoes
         JButton buttonCima = new JButton("↑");
         buttonCima.setPreferredSize(buttonSize);
 
@@ -55,6 +71,7 @@ public class Main {
 
         GridBagConstraints gbc = new GridBagConstraints();
 
+        //posiçoes das coisas no gui swing
         gbc.gridx = 1;
         gbc.gridy = 1;
         panel.add(buttonCima, gbc);
@@ -83,17 +100,22 @@ public class Main {
         gbc.insets = new Insets(10, 10, 0, 10);
         panel.add(messageLabel, gbc);
 
+        //definindo as especificações do frame
         frame.setSize(900,650);
         frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         frame.setTitle("Labirinto de Lex!");
         frame.getContentPane().setBackground(new Color(135, 248, 140));
 
+        //caixa de dialogo inicial
         JOptionPane.showMessageDialog(null, "BEM-VINDO AVENTUREIRO", "LABIRINTO", JOptionPane.INFORMATION_MESSAGE);
+
+        //input do tamanho do labirinto
         String stringLab = JOptionPane.showInputDialog("Digite o tamanho do labirinto que você deseja jogar: ");
         int tamanhoLab = Integer.parseInt(stringLab);
         Labirinto labirinto = new Labirinto(tamanhoLab);
         labirinto.gerarLabirinto();
 
+        //atribuindo ações para os botoes
         buttonCima.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
@@ -207,8 +229,10 @@ public class Main {
             }
         });
 
+        //adicionando o painel
         frame.add(panel);
 
+        //tornando o visivel
         frame.setVisible(true);
 
     }
